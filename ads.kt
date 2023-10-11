c11ae9491cf584dff3c6496294a409651cee32ac
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2023-10-11 22:01:53 +0700

    Update ads.kt

--- ads.kt
+++ ads.kt
@@ -6,4 +6,12 @@
   "ads_reward":"ca-app-pub-3940256099942544/5224354917",
   "ads_openapp":"ca-app-pub-3940256099942544/3419835294",
   "enable_ads": true
+},{
+  "package":"com.azzphucmasterdev.pro",
+  "ads_banner":"ca-app-pub-5074750635149619/9835592686",
+  "ads_inter":"ca-app-pub-5074750635149619/9644020999",
+  "ads_reward_inter":"ca-app-pub-5074750635149619/8189708213",
+  "ads_reward":"ca-app-pub-5074750635149619/8330939325",
+  "ads_openapp":"ca-app-pub-5074750635149619/6661004124",
+  "enable_ads": false
 }]
