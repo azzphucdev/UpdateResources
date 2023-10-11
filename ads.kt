a67af4239b6a84d4b830ce21d33b8d4ec16c3e8a
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2023-10-11 21:15:59 +0700

    Create ads.kt

---
+++ ads.kt
@@ -0,0 +1,9 @@
+[{
+  "package":"com.azzphucmasterdev.nsp",
+  "ads_banner":"ca-app-pub-3940256099942544/6300978111",
+  "ads_inter":"ca-app-pub-3940256099942544/1033173712",
+  "ads_reward_inter":"ca-app-pub-3940256099942544/5354046379",
+  "ads_reward":"ca-app-pub-3940256099942544/5224354917",
+  "ads_openapp":"ca-app-pub-3940256099942544/3419835294",
+  "enable_ads": true
+}]
