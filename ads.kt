610fb3d48a15fc601e614b45edfd1b5532d13a67
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2023-11-02 16:13:45 +0700

    Update ads.kt

--- ads.kt
+++ ads.kt
@@ -1,10 +1,10 @@
 [{
   "package":"com.azzphucmasterdev.nsp",
-  "ads_banner":"ca-app-pub-3940256099942544/6300978111",
-  "ads_inter":"ca-app-pub-3940256099942544/1033173712",
-  "ads_reward_inter":"ca-app-pub-3940256099942544/5354046379",
-  "ads_reward":"ca-app-pub-3940256099942544/5224354917",
-  "ads_openapp":"ca-app-pub-3940256099942544/3419835294",
+  "ads_banner":"ca-app-pub-5256385705854068/9387603140",
+  "ads_inter":"ca-app-pub-5256385705854068/1011802562",
+  "ads_reward_inter":"ca-app-pub-5256385705854068/95581213949",
+  "ads_reward":"ca-app-pub-5256385705854068/9872201766",
+  "ads_openapp":"ca-app-pub-5256385705854068/6072557550",
   "enable_ads": true
 },{
   "package":"com.azzphucmasterdev.pro",
