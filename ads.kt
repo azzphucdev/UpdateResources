18cadc1c9f0bae651a45f16564bba9e6213dc035
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2023-11-15 18:29:18 +0700

    Update ads.kt

--- ads.kt
+++ ads.kt
@@ -5,7 +5,7 @@
   "ads_reward_inter":"ca-app-pub-5256385705854068/95581213949",
   "ads_reward":"ca-app-pub-5256385705854068/9872201766",
   "ads_openapp":"ca-app-pub-5256385705854068/6072557550",
-  "isBaoTri1":"Bảo Trì [SplitData]Hmmmmmmm[SplitData]true",
+  "isBaoTri1":"Bảo Trì [SplitData]<span style='color:#FF0000;'>a</span><span style='color:#FF6600;'>z</span><span style='color:#FFCC00;'>z</span><span style='color:#CBFF00;'>p</span><span style='color:#65FF00;'>h</span><span style='color:#00FF00;'>u</span><span style='color:#00FF66;'>c</span> <span style='color:#00CBFF;'>b</span><span style='color:#0065FF;'>ả</span><span style='color:#0000FF;'>o</span> <span style='color:#CC00FF;'>t</span><span style='color:#FF00CB;'>r</span><span style='color:#FF0065;'>ì</span>[SplitData]false",
   "isBaoTri2":"",
   "enable_ads": true
 },{
