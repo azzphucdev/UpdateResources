f3036003a6b4fe5b036f5b729e2ea15ccacbca45
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2023-11-22 15:28:15 +0700

    Update ads.kt

--- ads.kt
+++ ads.kt
@@ -18,4 +18,14 @@
   "isBaoTri1":"",
   "isBaoTri2":"",
   "enable_ads": true
+},{
+  "package":"com.azzphucmasterdev.easypro",
+  "ads_banner":"ca-app-pub-5256385705854068/9387603140",
+  "ads_inter":"ca-app-pub-5256385705854068/1011802562",
+  "ads_reward_inter":"ca-app-pub-5256385705854068/95581213949",
+  "ads_reward":"ca-app-pub-5256385705854068/9872201766",
+  "ads_openapp":"ca-app-pub-5256385705854068/6072557550",
+  "isBaoTri1":"",
+  "isBaoTri2":"",
+  "enable_ads": true
 }]
